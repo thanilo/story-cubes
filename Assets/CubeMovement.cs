b9fb5f65e8cbe7d60098adbf9a0d37f472d16162
Thaís Lescano <thaislescano@hotmail.com>
2019-04-22 15:31:56 -0300

    só um teste

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+        
+    }
+}

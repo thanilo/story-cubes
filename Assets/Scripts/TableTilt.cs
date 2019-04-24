99b48e8b0ff0e5ea42361f759874c45d8c65d724
danilocaverna <nq3i4fsx@hotmail.com>
2019-04-24 13:57:20 -0400

    tilt da mesa com botao direito do mouse

---
+++ Assets/Scripts/TableTilt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableTilt : MonoBehaviour{
+    void Update(){
+        if(Input.GetMouseButtonDown(1) || Input.GetMouseButtonUp(1)){
+            FireTilt();
+        }
+    }
+    void FixedUpdate(){
+            transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y - transform.position.y * Time.deltaTime * 2f,
+            transform.position.z
+        );
+    }
+
+    void FireTilt(){
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y + 1f,
+            transform.position.z
+        );
+    }
+}

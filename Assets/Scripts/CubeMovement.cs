451989aa35c48362b3818deda4fb1fbd51702438
danilocaverna <nq3i4fsx@hotmail.com>
2019-04-24 13:28:05 -0400

    random positions script

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -2,24 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeMovement : MonoBehaviour
-{
+public class CubeMovement : MonoBehaviour{
     public float speed;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0, vertical);
 
         gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
-        
     }
 }
---
+++ Assets/Scripts/RandomPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour{
+    void Start(){
+        transform.rotation = Random.rotation;
+        transform.position = new Vector3(
+            transform.position.x + Random.Range(-1f, 1f),
+            transform.position.y + Random.Range(-1f, 1f),
+            transform.position.z + Random.Range(-1f, 1f)
+        );
+    }
+}
